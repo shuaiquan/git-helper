f013bf830c22b48502a941feb5bf26171904d170
shuaiquan <994011557@qq.com>
2019-04-09 09:22:50 +0800

    update clone

--- lib/Clone/index.ts
+++ lib/Clone/index.ts
@@ -1,28 +1,44 @@
 import { exec } from 'child_process';
+import * as path from 'path';
 
 const GIT_CLONE = 'git clone';
+const DEFUALT_DEST = './';
+const MASTER_BRANCH = 'master';
+
 
 interface CloneParams {
-    name: string;
     url: string;
+    name: string;
+    dest?: string;
     branch?: string;
 }
  
 async function clone(params: CloneParams) {
     const {
-        name,
         url,
-        branch = 'master',
+        dest = DEFUALT_DEST,
+        name,
+        branch = MASTER_BRANCH,
     } = params;
 
-    return new Promise((resolve, reject) => {
-        const cmdStr = `${GIT_CLONE} -b ${branch} ${url} ${name}`;
-        exec(cmdStr, (err) => {
+    return new Promise((resolve: (value: string) => void, reject) => {
+        const destUrl = path.resolve(process.cwd(), dest);
+        exec(`cd ${destUrl}`, (err) => {
             if (err) {
                 reject(err);
-            } else {
-                resolve();
             }
+            resolve(destUrl);
+        });
+    }).then((destUrl) => {
+        return new Promise((resolve, reject) => {
+            const cmdStr = `${GIT_CLONE} -b ${branch} ${url} ${name}`;
+            exec(cmdStr, (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(path.resolve(destUrl, `./${name}`));
+                }
+            });
         })
     });
 }
