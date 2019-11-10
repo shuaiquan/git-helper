2a887e9cb6a5481cbff14fa3574d98a6cb3830fc
shuaiquan <994011557@qq.com>
2019-11-10 16:27:19 +0800

    调整 tag.list 函数返回数组

--- lib/Tag/list.ts
+++ lib/Tag/list.ts
@@ -1,9 +1,11 @@
 import { execSync } from 'child_process';
 
 const TAG_LIST = 'git tag';
+const separator = /\s/;
 
 export function list(search?: string) {
     const cmdStr = search ? `${TAG_LIST} -l ${search}` : TAG_LIST;
-    const tag = execSync(cmdStr, { encoding: 'utf8' }).toString().trim();
-    return tag;
+    const tagStr = execSync(cmdStr, { encoding: 'utf8' }).toString().trim();
+    const tagArr = tagStr.split(separator)
+    return tagArr;
 }
